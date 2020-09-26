3d5457c694a0692709ac73c302a05d8bcdd37165
Ghassen Daoud <ghassen.daoud@insat.u-carthage.tn>
2020-09-26 12:59:16 +0100

    Updates

---
+++ precache-manifest.30da3c2b729bb0018f79380d3b278ae3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "713b359e24ec853f6b15936e58d5d920",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "42f0b329876874179221",
+    "url": "/monsters/static/css/main.60b471a0.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42f0b329876874179221",
+    "url": "/monsters/static/js/main.af6d52dc.chunk.js"
+  },
+  {
+    "revision": "03919b83b878cef951f1",
+    "url": "/monsters/static/js/runtime-main.cc0b5fe4.js"
+  }
+]);
